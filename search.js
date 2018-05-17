e4e60dbd779960700a7de8c4bcb2ae88a0455bb1
Georg M. Sorst <g.sorst@findologic.com>
2018-05-17 10:41:53 +0200

    Degrade search result quality

--- search.js
+++ search.js
@@ -5,7 +5,7 @@ const request = require('request-promise');
 const inputFile = 'hits-full.json';
 
 const solrHostAndCore = 'http://172.17.0.1:8983/solr/gettingstarted';
-const qf = 'title_txt_en^10 headings_txt_en^5 categories_txts_en^2 opening_text_txt_en^2 text_txt_en^2';
+const qf = 'title_txt_en headings_txt_en categories_txts_en opening_text_txt_en text_txt_en';
 
 let wikiClient = wiki({apiUrl: 'https://simple.wikipedia.org/w/api.php'});
 
