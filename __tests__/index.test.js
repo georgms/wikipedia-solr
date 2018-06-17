781bb6910a9700f4c5bb58728e1229a821582cfe
Georg M. Sorst <g.sorst@findologic.com>
2018-06-17 11:47:40 +0200

    Improve test name

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,17 +1,19 @@
 const sampleDocs = require('./sampledocs.test.json');
 
-describe('Index', () => {
-    it('should have called the fetch function wih the good const parameter and slug', done => {
-
+describe('Indexing', () => {
+    it('should be called with the correct Solr data', (done) => {
 
         jest.mock('request-promise', () => {
             return jest.fn((input) => {
-                return Promise.resolve('something');
+                return Promise.resolve();
             });
         });
+
         const indexMethods = require('../index');
+
         indexMethods.readAndImportFiles('./__tests__/simplewiki.test.json', 2).then(() => {
             let requestSpy = require('request-promise');
+
             expect(requestSpy).toHaveBeenCalledTimes(2);
 
             let firstCall = requestSpy.mock.calls[0][0];
@@ -21,6 +23,7 @@ describe('Index', () => {
             let secondCall = requestSpy.mock.calls[1][0];
             expect(secondCall.url).toEqual('http://172.17.0.1:8983/solr/gettingstarted/update/json/docs?commitWithin=10000&overwrite=true&wt=json');
             expect(secondCall.json).toEqual([sampleDocs[2]]);
+
             done();
         });
     });
